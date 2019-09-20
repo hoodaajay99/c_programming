072a32d61f4e218f9bc0484e3798eb5d6e878f32
Ajay Hooda <hoodaajay@gmail.com>
2019-09-20 16:23:19 +0530

    added cname

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'C LANGUAGE COURSE', // Title for your website.
   tagline: 'Learn Programming in C',
   url: 'https://hoodaajay99.github.io', // Your website URL
-  baseUrl: '/c_programming/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
@@ -106,6 +106,8 @@ const siteConfig = {
   //   repoUrl: 'https://github.com/facebook/test-site',
 
   // customDocsPath: '../c_programming_course'
+
+  cname: 'techskillo.com'
 };
 
 module.exports = siteConfig;
